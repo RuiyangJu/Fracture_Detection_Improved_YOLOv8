b0a288a4aa4150c4444516e53b50c16606d23bd8
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-11-25 20:06:45 +0800

    Add files via upload

---
+++ start_train.py
@@ -0,0 +1,17 @@
+import argparse
+from ultralytics import YOLO
+
+def parse_args():
+    parse = argparse.ArgumentParser(description='Data Postprocess')
+    parse.add_argument('--model', type=str, default=None, help='load the model')
+    parse.add_argument('--data_dir', type=str, default=None, help='the dir to data')
+    args = parse.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+    model = YOLO(args.model)
+    model.train(args.data_dir)
+
+if __name__ == '__main__':
+    main()
